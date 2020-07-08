5cae4cdb91e8f8e37e7ab602ae44555cfd4d5221
付星豪 <fxh18574134304@163.com>
2020-07-08 14:03:33 +0800

    blueBooth 第九次

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -243,7 +243,7 @@ class BluetoothTerminal {
 
     // filters: [{ services: [this._serviceUuid] }, { namePrefix: 'i' }],
     return navigator.bluetooth.requestDevice({
-      filters: [{ services: [this._serviceUuid] }, { namePrefix: '小' }],
+      filters: [{ services: [this._serviceUuid] }, { namePrefix: 'A' }],
     }).
       then((device) => {
         this._log('"' + device.name + '" bluetooth device selected');
